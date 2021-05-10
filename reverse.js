10816b78678462a8e793c661eaafd52368498df6
Kevin Pierce <piercekg@gmail.com>
2021-05-10 17:22:13 -0600

    recursion

---
+++ reverse.js
@@ -0,0 +1,21 @@
+/*
+Write a recursive function which accepts a string and returns a new string in reverse.
+*/
+
+var reverse = function(str) {
+  var helper = function(arr1, arr2) {
+    if (arr1.length === 0) {
+      return arr2;
+    }
+    arr2.unshift(arr1.shift());
+    return helper(arr1, arr2);
+  }
+  var reverseStr = helper(str.split(''), []);
+  return reverseStr.join('');
+};
+/*
+var result = reverse('awesome'); // 'emosewa'
+console.log(result);
+var result = reverse('rithmschool'); // 'loohcsmhtir'
+console.log(result);
+*/
