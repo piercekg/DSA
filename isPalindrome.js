ed4f013fe4d3ddd796a4c4732d3778945c29bbe5
Kevin Pierce <piercekg@gmail.com>
2021-05-10 17:57:51 -0600

    sdf

--- isPalindrome.js
+++ isPalindrome.js
@@ -3,6 +3,19 @@ Write a recursive function which returns true if the string passed to it is a pa
 */
 
 var isPalindrome = function(str) {
+  if (str.length === 1) {
+    return true;
+  }
+  if (str.length === 2) {
+    return str[0] === str[1];
+  }
+  if (str[0] === str.slice(-1)) {
+    return isPalindrome(str.slice(1, -1));
+  }
+  return false;
+};
+/*
+var isPalindrome = function(str) {
   var reverse = function(arr1, arr2) {
     if (arr1.length === 0) {
       return arr2.join('');
@@ -15,6 +28,7 @@ var isPalindrome = function(str) {
   }
   return false;
 };
+*/
 /*
 var result = isPalindrome('awesome'); // false
 console.log(result);
--- reverse.js
+++ reverse.js
@@ -3,6 +3,13 @@ Write a recursive function which accepts a string and returns a new string in re
 */
 
 var reverse = function(str) {
+  if (str.length <= 1) {
+    return str;
+    return reverse(str.slice(1)) + str[0];
+  }
+};
+/*
+var reverse = function(str) {
   var helper = function(arr1, arr2) {
     if (arr1.length === 0) {
       return arr2;
@@ -13,6 +20,7 @@ var reverse = function(str) {
   var reverseStr = helper(str.split(''), []);
   return reverseStr.join('');
 };
+*/
 /*
 var result = reverse('awesome'); // 'emosewa'
 console.log(result);
