a9cf2a87f5f8dcf0a4bfda23e29c4c18de65da7b
Kevin Pierce <piercekg@gmail.com>
2021-05-10 17:29:07 -0600

    recursion

---
+++ isPalindrome.js
@@ -0,0 +1,29 @@
+/*
+Write a recursive function which returns true if the string passed to it is a palindrome; otherwise, return false.
+*/
+
+var isPalindrome = function(str) {
+  var reverse = function(arr1, arr2) {
+    if (arr1.length === 0) {
+      return arr2.join('');
+    }
+    arr2.unshift(arr1.shift());
+    return reverse(arr1, arr2);
+  }
+  if (str === reverse(str.split(''), [])) {
+    return true;
+  }
+  return false;
+};
+/*
+var result = isPalindrome('awesome'); // false
+console.log(result);
+var result = isPalindrome('foobar'); // false
+console.log(result);
+var result = isPalindrome('tacocat'); // true
+console.log(result);
+var result = isPalindrome('amanaplanacanalpanama'); // true
+console.log(result);
+var result = isPalindrome('amanaplanacanalpandemonium'); // false
+console.log(result);
+*/
