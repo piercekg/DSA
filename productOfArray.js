ed17a38e6736e825462148e67802aa6534f33fdf
Kevin Pierce <piercekg@gmail.com>
2021-05-10 16:51:20 -0600

    recursion

---
+++ productOfArray.js
@@ -0,0 +1,17 @@
+/*
+Write a function which takes in an array of numbers and returns the product of them all.
+*/
+
+var productOfArray = function(arr) {
+  if (arr.length === 0) {
+    return 1;
+  }
+  var num = arr.shift();
+  return num * productOfArray(arr);
+};
+/*
+var result = productOfArray([1,2,3]); // 6
+console.log(result);
+var result = productOfArray([1,2,3,10]); // 60
+console.log(result);
+*/
